287d652ecbe9569047488bef0c5fdd2637540eaa
Goddy-2024 <gaoajuoga@gmail.com>
2025-03-03 20:10:04 +0300

    branches and cloning

--- tanzania.py
+++ tanzania.py
@@ -3,3 +3,5 @@ print("Jambo Tanzania")
 print("teacher you are too fast")
 
 print('clone - to have someones repo in your machiene - to download')
+
+print('about branches git checkout new-branch>>> switches to a new branch')
